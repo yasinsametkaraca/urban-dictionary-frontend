863060a46f59b549e3f8f82ee9a1bbad1722129e
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2022-11-04 22:56:36 +0300

    local storage added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,34 @@
 import {createStore} from "redux";
 import authReducer from "./authReducer"
+import SecureLS from "secure-ls";
 
-const loggedInState = {
-    isLoggedIn:true,
-    username:"user1",
-    displayName:"display1",
-    image:null,
-    password:"Deneme.38"
-};
+const secureLs = new SecureLS()  //local storage e aktaracağımız state bilgilerini şifreleyerek aktarmak için
 
 const configureStore = () => {
-    return createStore(authReducer,loggedInState);
+
+    /*const authData = localStorage.getItem("auth-data")*/
+    const authData = secureLs.get("auth-data")
+
+    let stateInLocalStorage = {   //login olunmamışsa burası çalışır. Undefined tanımlandığı için javascript localStorage e bu bilgileri koymaz.
+        isLoggedIn:false,
+        username:undefined,
+        displayName:undefined,
+        image:undefined,
+        password:undefined
+    }
+    if(authData){
+            /*stateInLocalStorage = JSON.parse(authData)*/   //sayfayı yenilesek dahi eğer kullanıcı giriş yapmışsa state girili haliyle kalır.
+            stateInLocalStorage = authData
+    }
+
+    const store = createStore(authReducer,stateInLocalStorage,window.REDUX_DEVTOOLS_EXTENSION_ );
+
+    store.subscribe(()=>{                           //storeda her değişim olduğunda burası çağrılır.
+        /*localStorage.setItem("auth-data",JSON.stringify(store.getState()))*/
+        secureLs.set("auth-data",store.getState())
+    })
+    
+    return store;
 }
 
 export default configureStore
